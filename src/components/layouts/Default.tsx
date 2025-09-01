90ec89b8201ed19d0fe3bc6f095b8a53f218e8aa
cansoup <0311minha@gmail.com>
2025-09-02 09:56:04 +1000

    feat: mobile opt

--- src/components/layouts/Default.tsx
+++ src/components/layouts/Default.tsx
@@ -6,7 +6,7 @@ import Footer from "../Footer.tsx";
 
 const DefaultLayout = () => {
     return (
-        <div className="py-8 px-16 relative h-screen">
+        <div className="px-4 py-8 md:px-16 relative h-screen">
             <MyInfo/>
             <Menus/>
             <div className="flex justify-end mt-20">
