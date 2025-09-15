98655269137bacf9c8d25b84ab75265af9981441
cansoup <0311minha@gmail.com>
2025-09-15 12:52:10 +1000

    feat: making constants of my info

---
+++ src/constants/certificates.ts
@@ -0,0 +1,8 @@
+export const certifications = [
+    {
+        title: "Engineer Information Processing",
+        period: "16.08.2019",
+        institution: "Human Resources Development Service of Korea",
+        description: "A national technical qualification recognizing expertise in the IT field, including software development, database construction, and system analysis and design."
+    },
+];
---
+++ src/constants/skillsets.ts
@@ -0,0 +1,7 @@
+export const skillSets = {
+    "Languages": ["JavaScript (ES6+)", "TypeScript", "HTML5", "CSS3"],
+    "Frameworks & Libraries": ["React", "Next.js", "Vue.js"],
+    "State Management": ["Zustand", "React Query"],
+    "Styling": ["Tailwind CSS", "Styled-components", "Sass"],
+    "Tools & Etc": ["Git", "Webpack", "Figma"],
+};
